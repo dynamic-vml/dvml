01e38a854b486f9c37bf845a6ce427497f1432ab
Cesar de Souza <cesarsouza@gmail.com>
2020-05-12 18:45:48 +0200

    Refactoring the javascript scripts to ease testing

--- src/wwwroot/lib/dynamic-viewmodel-lists/dvml.js
+++ src/wwwroot/lib/dynamic-viewmodel-lists/dvml.js
@@ -2,45 +2,65 @@
 
     'use strict';
 
+    // Polyfill for string.startsWith
+    if (!String.prototype.startsWith) {
+        Object.defineProperty(String.prototype, 'startsWith', {
+            value: function (search, rawPos) {
+                var pos = rawPos > 0 ? rawPos | 0 : 0;
+                return this.substring(pos, pos + search.length) === search;
+            }
+        });
+    }
+
     function add(containerId, actionUrl) {
         var container = $('#' + containerId);
+        var requestOptions = null;
+
+        var onSuccess = function (response) {
+            if (response.success) {
+                container.append(response.html);
+            }
+        };
 
         if (actionUrl.startsWith("POST")) {
-            var parts = actionUrl.split("|");
-            var action = parts[1];
-            var json = parts[2];
-            $.ajax({
-                url: action,
-                data: json,
+            // We will make an ajax request via POST
+            var actionSettings = actionUrl.split("|");
+
+            requestOptions = {
+                url: actionSettings[1],
+                data: actionSettings[2],
                 contentType: "application/json; charset=utf-8",
                 dataType: "json",
                 type: "POST",
                 cache: false,
-                success: function (response) {
-                    if (response.success) {
-                        container.append(response.html);
-                    }
-                }
-            });
+                success: onSuccess
+            }
         }
         else {
-            $.ajax({
+            // We will make an ajax request via GET
+            requestOptions = {
                 url: actionUrl,
                 type: "GET",
                 cache: false,
-                success: function (html) {
-                    container.append(html);
-                }
-            });
+                success: onSuccess
+            }
         }
+
+        // Perform the request
+        $.ajax(requestOptions); 
+
+        return {
+            container: container[0], // return information about the request
+            options: requestOptions  // to enable better testing / debugging.
+        };  
     };
 
     function remove(itemId) {
-        $('#' + itemId).remove();
+        return $('#' + itemId).remove();
     };
 
     return {
         add: add,
-        remove: remove
+        remove: remove,
     };
 })();
